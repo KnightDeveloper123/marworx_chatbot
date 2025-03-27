c690df3a082e774ae9ec6e983e932af5371f3509
sushil patil <sushilpatil1085@gmail.com>
2025-03-27 15:08:07 +0530

    Crud for user

--- backend/routes/users/user.js
+++ backend/routes/users/user.js
@@ -1,51 +1,155 @@
-const express = require('express');
-const router = express.Router();
-const connection = require('../../database/db');
+const express = require("express");
+const connection = require("../../database/db.js");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { middleware } = require("../../middleware/middleware");
+const executeQuery = require('../../utils/executeQuery');
+const { addUserSchema, updateUserSchema, deleteUserSchema } = require("../../validation/user.js");
 
-router.post("/loginuser", (req, res) => {
-    const { emailid, password } = req.body;
 
-    const sql = "SELECT * FROM users WHERE emailid=?";
-    const value = [emailid];
 
-    connection.query(sql, value, (err, result) => {
+const router=express.Router();
 
-        const user = result[0];
-        if (err) {
-            console.error("Error adding user:", err);
-            return res.status(500).json({ error: "Database error" });
+
+// router.post("/login", async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+
+//         const checkEmail = await executeQuery(`select * from user where email=?`, [email]);
+
+//         if (!checkEmail[0]?.password) {
+//             return res.status(400).json({ error: "Please set your password" })
+//         }
+
+//         if (!checkEmail[0] || !password) {
+//             return res.status(400).json({ error: "Invalid Credentials" });
+//         }
+//         const pwdCompare = await bcrypt.compare(password, checkEmail[0].password);
+
+//         if (!pwdCompare) {
+//             return res.status(400).json({ error: "Invalid Credentials" })
+//         }
+
+//         if (pwdCompare) {
+//             const payload = {
+//                 email: email,
+//                 user_id: checkEmail[0].id,
+//                 user_type: checkEmail[0].role
+//             };
+//             let auth_token = jwt.sign(payload, process.env.JWT_SECRET);
+//             await executeQuery(`update user set last_login=NOW() where id=${checkEmail[0]?.id};`)
+//             return res.json({ success: `Welcome Back, ${checkEmail[0]?.name}`, data: { name: checkEmail[0]?.name, email: checkEmail[0]?.email, role: checkEmail[0].role, id: checkEmail[0].id }, auth_token })
+//         } else {
+//             return res.status(400).json({ error: "Invalid Credentials." });
+//         }
+//     } catch (error) {
+//         console.log("auth/user/login: ", error.message);
+//         return res.status(500).json({ error: "Internal Server Error." });
+//     }
+// });
+
+router.post("/signUp", async (req, res) => {
+    try {
+        const { name, email,password, mobile_no } = req.body;
+
+        var salt = bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hash(password, salt);
+        console.log(secPass);
+        
+        
+
+        const { error } = addUserSchema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            return res.status(400).json({ error: error.details[0]?.message });
         }
-        if (result.length > 0) {
-            if (bcrypt.compareSync(password, result[0].password)) {
-                const { password: _, ...userData } = user;
-                res.status(201).json({ message: "User logged in successfully", user: userData });
-            } else {
-                res.status(401).json({ message: "Invalid credentials" });
+
+        const [checkEmail] = await executeQuery(`select * from user where email=?`, [email])
+        if (checkEmail) {
+            return res.status(400).json({ error: "Email already exist" })
+        }
+
+        const insertQuery = 'insert into user (name, email,password, mobile_no) values (?, ?, ?, ?);'
+        connection.execute(insertQuery, [name, email,secPass, mobile_no], (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ error: "Something went wrong" })
             }
-        } else {
-            res.status(401).json({ message: "Invalid credentials" });
-        }
-    })
-
+            return res.json({ success: "User Added", data })
+        })
+    } catch (error) {
+        console.log("auth/addUser: ", error.message);
+        return res.status(500).json({ error: "Internal Server Error." });
+    }
 });
 
-
-router.post("/adduser", (req, res) => {
-    const { username, emailid, password } = req.body;
-
-    const hashPassword = bcrypt.hashSync(password, 10);
-
-    const sql = "INSERT INTO users (username,emailid,password) VALUES (?,?,?)";
-    const value = [username, emailid, hashPassword];
-
-    connection.query(sql, value, (err, result) => {
-        if (err) {
-            console.error("Error adding user:", err);
-            return res.status(500).json({ error: "Database error" });
+router.post("/updateUser", async (req, res) => {
+    try {
+        const { user_id,password, ...rest } = req.body;
+        const { error } = updateUserSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            return res.status(400).json({ error: error.details.map(err => err.message) });
         }
-        res.status(201).json({ message: "User added successfully" });
-    })
 
+        if (password) {
+            const salt = bcrypt.genSaltSync(10);
+            const secPass = await bcrypt.hash(password, salt);
+            rest.password = secPass; // Replace plaintext password with hashed password
+        }
+
+        const fields = Object.keys(rest);
+        const values = Object.values(rest);
+
+        if (fields.length === 0) {
+            return res.status(400).json({ error: "No valid fields provided for update." });
+        }
+
+        const setClause = fields.map(field => `${field} = ?`).join(", ");
+        values.push(user_id);
+
+        const query = `UPDATE user SET ${setClause}, updated_at = CURRENT_TIMESTAMP WHERE id = ?`;
+
+        connection.execute(query, values, (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ error: "Something went wrong" })
+            }
+            if (data.affectedRows === 0) {
+                return res.status(404).json({ error: "Record not found" });
+            }
+            return res.json({ success: "User updated", data })
+        });
+    } catch (error) {
+        console.error("Error in /updateUser :", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-module.exports = router;
+router.post("/deleteUser", async (req, res) => {
+    try {
+        const { user_id } = req.body;
+
+        const { error } = deleteUserSchema.validate(req.body, { abortEarly: false });
+        if (error) {
+            return res.status(400).json({ error: error.details.map(err => err.message) });
+        }
+
+        const query = `UPDATE user SET status=1, updated_at = CURRENT_TIMESTAMP WHERE id = ?`;
+
+        connection.execute(query, [user_id], (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).json({ error: "Something went wrong" })
+            }
+            if (data.affectedRows === 0) {
+                return res.status(404).json({ error: "Record not found" });
+            }
+            return res.json({ success: "User deleted", data })
+        });
+    } catch (error) {
+        console.error("Error in /updateUser:", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+module.exports=router;
---
+++ backend/validation/user.js
@@ -0,0 +1,71 @@
+const Joi = require('joi');
+
+const addUserSchema = Joi.object({
+    name: Joi.string().required().max(255).messages({
+        'string.base': 'Name must be a string.',
+        'any.required': 'Name is required'
+    }),
+
+    mobile_no: Joi.string().required().messages({
+        'string.email': 'Email must be a valid email address.',
+        'any.email': 'Name is required'
+    }),
+
+    email: Joi.string().required().email().messages({
+        'string.email': 'Email must be a valid email address.',
+        'any.email': 'Name is required'
+    }),
+    password: Joi.string().min(4).max(50).required().messages({
+        "string.min": "Password must be at least 4 characters long.",
+        "string.max": "Password must not exceed 50 characters.",
+        "any.required": "Password is required."
+    }),
+
+    date_of_birth: Joi.date().iso().less('now').messages({
+        'date.base': 'Date of Birth must be a valid date.',
+        'date.format': 'Date of Birth must be in YYYY-MM-DD format.',
+        'date.less': 'Date of Birth must be in the past.'
+    })
+});
+
+const updateUserSchema = Joi.object({
+    name: Joi.string().optional().max(255).allow(null, '').messages({
+        'string.base': 'Name must be a string.',
+    }),
+
+    email: Joi.string().optional().email().allow(null, '').messages({
+        'string.email': 'Email must be a valid email address.',
+    }),
+    mobile_no: Joi.string().optional().messages({
+        'string.email': 'Email must be a valid email address.',
+        'any.email': 'Name is required'
+    }),
+    password: Joi.string().min(4).max(50).required().messages({
+        "string.min": "Password must be at least 4 characters long.",
+        "string.max": "Password must not exceed 50 characters.",
+        "any.required": "Password is required."
+    }),
+
+    date_of_birth: Joi.date().iso().less('now').optional().allow(null, '').messages({
+        'date.base': 'Date of Birth must be a valid date.',
+        'date.format': 'Date of Birth must be in YYYY-MM-DD format.',
+        'date.less': 'Date of Birth must be in the past.'
+    }),
+
+    user_id: Joi.number().required().messages({
+        'number.base': "User should be int",
+        'any.required': 'User is required'
+    }),
+    status: Joi.number().optional().allow(null, '').messages({
+        'number.base': 'User Status is invalid.',
+    }),
+});
+
+const deleteUserSchema = Joi.object({
+    user_id: Joi.number().required().messages({
+        'number.base': 'Invalid User',
+        'any.required': 'User is required'
+    })
+})
+
+module.exports = { addUserSchema, updateUserSchema, deleteUserSchema };
