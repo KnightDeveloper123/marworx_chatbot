0d7666453a807dfabad2e36093540fb943bf2583
KnightDeveloper123 <developer2@knightmotion.com>
2025-03-27 14:13:34 +0530

    Merge branch 'main' of https://github.com/KnightDeveloper123/marworx_chatboat

---
+++ src/components/context/AppContext.jsx
@@ -0,0 +1,64 @@
+import { useToast } from "@chakra-ui/react";
+import React, { createContext, useCallback, useMemo, useRef, useState } from "react";
+export const AppContext = createContext();
+
+const APP_URL = import.meta.env.VITE_BACKEND_URL
+
+export const AppProvider = ({ children }) => {
+
+    const token = localStorage.getItem('token');
+
+    const toast = useToast();
+    const statusMap = {
+        success: "success",
+        danger: "error",
+        warning: "warning",
+        info: "info"
+    };
+    const showAlert = (message, type) => {
+        toast({
+            title: message,
+            status: statusMap[type] || "error",
+            duration: 4000,
+            isClosable: true,
+            position: 'top'
+        })
+    }
+
+    const [loading, setLoading] = useState(false)
+
+    const [employee, setEmployee] = useState([]);
+    const fetchAllEmployee = async () => {
+        try {
+            setLoading(true)
+            const response = await fetch(`${APP_URL}/employee/getAllEmployee`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                }
+            })
+            const result = await response.json();
+            if (result.success) {
+                setEmployee(result.data)
+            } else {
+                showAlert(result.error, "error")
+            }
+        } catch (error) {
+            console.log(error);
+            showAlert("Internal Server Error!", "error")
+        }
+    }
+
+    return (
+        <AppContext.Provider
+            value={{
+                showAlert, loading,
+                fetchAllEmployee, employee,
+            }}
+        >
+            {children}
+        </AppContext.Provider>
+    )
+
+}
