7548a0b0632d5b07f00d9c89ea3eee10e60d365d
KnightDeveloper123 <KnightDeveloper123.com>
2025-03-27 17:22:55 +0530

    work on employee

--- src/components/admin/Employee.jsx
+++ src/components/admin/Employee.jsx
@@ -1,84 +1,353 @@
-import { Box, Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,useDisclosure
+} from "@chakra-ui/react";
+import React, { useContext, useEffect,} from "react";
+import { AppContext } from "../context/AppContext";
+import Card from "../../Card";
+import { IoMdAdd } from "react-icons/io";
+import { RxDotsHorizontal } from "react-icons/rx";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { DeleteIcon } from "@chakra-ui/icons";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 function Employee() {
-
-  const [employee, setEmployee] = useState([]);
-
-  const token = localStorage.getItem("token");
-
-
-  const navigate=useNavigate();
-
-  const getEmployee = async () => {
-    try {
-
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/employee/getAllEmployee`, {
-        headers: {
-          Authorization: token
-        }
-      });
-      const result = await response.json();
-
-      setEmployee(result.data)
-      console.log(result)
-    } catch (error) {
-      console.log(error)
-    }
-
-
-  }
-
+  const { fetchAllEmployee, employee, formatDate } = useContext(AppContext);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const { register, handleSubmit, formState: { errors } } = useForm();
   useEffect(() => {
-    getEmployee();
-  }, [])
+    fetchAllEmployee();
+  }, []);
 
   return (
-    <Box width={'100%'} p={'10px'}>
+    <Card>
+      <Flex
+        w="100%"
+        justifyContent="space-between"
+        flexDirection={"column"}
+        p="5px"
+      >
+        <Flex
+          w="100%"
+          alignItems={"center"}
+          justifyContent="space-between"
+          gap="10px"
+        >
+          <Text fontWeight="var(--big-font-weight)" fontSize="var(--semi-big)">
+            Employee
+          </Text>
+          <Flex gap={2}>
+            {/* <InputGroup alignItems="center">
+            <InputLeftElement pointerEvents="none" top='auto'>
+              <SearchIcon />
+            </InputLeftElement>
+            <Input
+              type="text"
+              placeholder="Name, Lead status, Source..."
+              fontSize="var(--mini-text)"
+              fontWeight="var(--big-font-weight)"
+              // height={{ base: "28px", md: "30px", lg: "40px", xl: "40px" }}
+              size='sm'
+              name="currency"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              bgColor={"#F2F4F800"}
+              border='1px solid #858585'
+              borderRadius="var(--radius)"
+            />
+            <InputRightElement top='auto'>
+              <TbFilterCancel onClick={clearFilter} color="green.500" />
+            </InputRightElement>
+          </InputGroup> */}
 
-      <Box display={'flex'} justifyContent={'end'} >
-        <Button bgColor='var(--active-bg)' textColor={'white'} _hover={''} _active={''}>Add Employee</Button>
-      </Box>
-
-      <Box>
-        <TableContainer >
-          <Table border={'1px solid black'} size={'sm'} width={'100%'} margin={'auto'} mt={'20px'} p={'10px'}>
-            <Thead>
-              <Tr>
-                <Th border={'1px solid black'}>Name</Th>
-                <Th border={'1px solid black'} >Email</Th>
-                <Th border={'1px solid black'}>Phone No</Th>
-                <Th border={'1px solid black'}>DOB </Th>
-                <Th border={'1px solid black'}>role</Th>
-                <Th border={'1px solid black'}>Last login</Th>
-
-                <Th border={'1px solid black'}>Action</Th>
+            <Flex
+            // display={location.pathname === "/admin/dashboard" ? "none" : "Flex"}
+            >
+              {/* {userDetails.type === "admin" || userDetails.active === 1 ? ( */}
+              <Button
+                borderRadius="var(--radius)"
+                leftIcon={<IoMdAdd fontSize={"20px"} />}
+                _hover={{ bgColor: "var(--active-bg)" }}
+                bgColor="var(--active-bg)"
+                color="#fff"
+                h={"35px"}
+                fontSize="var(--mini-text)"
+                fontWeight="var(--big-font-weight)"
+                onClick={() => onOpen()} 
+              >
+                Add Employee
+              </Button>
+              {/* 
+            ) : (
+              ""
+            )} */}
+            </Flex>
+          </Flex>
+        </Flex>
+        <TableContainer
+          mt="20px"
+          borderRadius="5px 5px 0px 0px"
+          //  maxH={flag ? "unset" : "600px"}
+          // overflowY={flag ? "unset" : "scroll"}
+        >
+          <Table size="sm" className="custom-striped-table">
+            <Thead border="0.5px solid #F2F4F8">
+              <Tr h="40px" bgColor="var(--table-header-bg)">
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius="5px 0px 0px 0px"
+                  fontSize="var(--mini-text)"
+                >
+                  ID
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  name
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  email
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  Phone No
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  DOB
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  role
+                </Th>
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius=""
+                  fontSize="var(--mini-text)"
+                >
+                  Last login
+                </Th>
+                {/* {userDetails.type === "admin" || userDetails.active === 1 ? ( */}
+                <Th
+                  fontWeight="var(--big-font-weight)"
+                  color="var(--text-black)"
+                  borderRadius="0px 5px 0px 0px"
+                  fontSize="var(--mini-text)"
+                >
+                  Actions
+                </Th>
+                {/* ) : (
+              ""
+            )} */}
               </Tr>
             </Thead>
+
             <Tbody>
-
-              {
-                employee && employee.map((emp) => (
-
-                  <Tr key={emp.id}>
-                    <Td border={'1px solid black'}>{emp.name}</Td>
-                    <Td border={'1px solid black'}>{emp.email}</Td>
-                    <Td border={'1px solid black'}>{emp.mobile_no}</Td>
-                    <Td border={'1px solid black'}>{emp.date_of_birth}</Td>
-                    <Td border={'1px solid black'}>{emp.role}</Td>
-                    <Td border={'1px solid black'}>{emp.last_login ? new Date(emp.last_login).toISOString().split('T')[0] : ''}</Td>
-                    <Td border={'1px solid black'}>edit/delete</Td>
+              {employee &&
+                employee.map((d, index) => (
+                  <Tr
+                    key={index}
+                    border="0.5px solid #F2F4F8"
+                    h="40px"
+                    textAlign="start"
+                  >
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      E-{d.id}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                      // onClick={() => editleads(d.id)} _hover={{ cursor: "pointer", color: "navy" }}
+                    >
+                      {d.name}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      {d.email}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      {d.mobile_no}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      {d.date_of_birth}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      {d.role}
+                    </Td>
+                    <Td
+                      border="0.5px solid #F2F4F8"
+                      color={"#404040"}
+                      fontSize="var(--mini-text)"
+                      fontWeight="var(--big-font-weight)"
+                    >
+                      {formatDate(d.last_login)}
+                    </Td>
+                    <Td border="0.5px solid #F2F4F8" color={"#404040"} fontSize="var(--mini-text)">
+                      <Menu >
+                        <MenuButton
+                          bgColor="transparent"
+                          _hover={{ bgColor: "transparent", color: "var(--active-bg)" }}
+                          _active={{ bgColor: "transparent", color: "var(--active-bg)" }}
+                          as={Button}
+                        >
+                          <RxDotsHorizontal />
+                        </MenuButton>
+                        <MenuList gap={2} >
+                         <MenuItem
+                            w="100%"
+                            minW="100px"
+                            // onClick={() => editleads(d.id)}
+                            display={'flex'} alignItems={'center'} gap={2}
+                          >
+                            <MdOutlineModeEdit color="green" />
+                            <Text fontSize="var(--mini-text)" fontWeight="var(--big-font-weight)">
+                              Edit
+                            </Text>
+                          </MenuItem>
+                          <Divider />
+                          <MenuItem
+                            w="100%"
+                            minW="100px"
+                            cursor="pointer"
+                            // onClick={() => {
+                            //   setSelectLead(d);
+                            //   dellead(d.id);
+                            // }}
+                          >
+                            <Flex gap={2} alignItems="center">
+                              <DeleteIcon color={"red"} />
+                              <Text>Delete</Text>
+                            </Flex>
+                          </MenuItem>
+                        </MenuList>
+                      </Menu>
+                    </Td>
                   </Tr>
-                ))
-              }
-
+                ))}
             </Tbody>
           </Table>
         </TableContainer>
-      </Box>
-    </Box>
-  )
+      </Flex>
+      <Modal  isOpen={isOpen}
+        onClose={onClose} >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader fontSize={'18px'}>Add Employee</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+
+            <Box  as='form' display={'flex'} flexDirection={'column'} gap={'8px'}>
+              <FormControl>
+                <FormLabel fontSize="var(--mini-text)" mb={'2px'}>Name</FormLabel>
+                <Input type='text' {...register('name', { required: 'Name is required' })}
+                  placeholder='Enter your name' fontSize="var(--text-12px)" autoComplete='off'></Input>
+                {errors.name && <Text>{errors.name.message}</Text>}
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize="var(--mini-text)" mb={'2px'}>Email</FormLabel>
+                <Input type='email' {...register('email', { required: 'Email is required' })}
+                  placeholder='Enter email' fontSize="var(--text-12px)" autoComplete='off'></Input>
+                {errors.email && <Text>{errors.email.message}</Text>}
+              </FormControl>
+              <FormControl>
+                <FormLabel fontSize="var(--mini-text)" mb={'2px'}>Phone No</FormLabel>
+                <Input type='number' {...register('mobile_no', { required: 'Phone no is required' })}
+                  placeholder='Enter Phone no' fontSize="var(--text-12px)" autoComplete='off'></Input>
+                {errors.mobile_no && <Text>{errors.mobile_no.message}</Text>}
+              </FormControl>
+
+              <FormControl>
+                <FormLabel fontSize="var(--mini-text)" mb={'2px'} >DOB</FormLabel>
+                <Input type='date' {...register('date_of_birth', { required: "DOB is required" })} fontSize="var(--text-12px)"  ></Input>
+                {errors.date_of_birth && <Text>{errors.date_of_birth.message}</Text>}
+              </FormControl>
+          </Box>
+          </ModalBody>
+
+          <ModalFooter display={'flex'} alignItems={'center'} justifyContent={'center'} gap={'6px'}>
+            <Button type='submit' fontSize={'13px'} bgColor={'#FF5722'} _hover={''} textColor={'white'} size={'sm'}>
+              Save
+            </Button>
+            <Button onClick={onClose} type="button" size={'sm'} fontSize={'13px'} border={'1px solid #FF5722 '}
+              textColor={'#FF5722'} bgColor={'white'} mr={3} _hover={''}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Card>
+  );
 }
 
-export default Employee
+export default Employee;
