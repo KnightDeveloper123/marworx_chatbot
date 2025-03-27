6e5293b79e3b83baf5d11403133afa67be45db26
KnightDeveloper123 <developer2@knightmotion.com>
2025-03-27 11:35:46 +0530

    Project Setupt

---
+++ backend/database/db.js
@@ -0,0 +1,20 @@
+var mysql2 = require('mysql2')
+var connection = mysql2.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'chatbot',
+})
+
+
+connection.getConnection((err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Database connected')
+})
+
+
+
+module.exports = connection;
---
+++ backend/middleware/middleware.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+const middleware = (req, res, next) => {
+    try {
+        const token =
+            req.body.token ||
+            req.header("Authorization")
+
+        if (!token || token === undefined) {
+            return res.status(401).json({
+                success: false,
+                message: "Token Missing",
+            });
+        }
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decode;
+        } catch (err) {
+            return res.status(401).json({
+                success: false,
+                message: "token is invalid",
+            });
+        }
+
+        next();
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({
+            success: false,
+            message: "Something went wrong while verifying token",
+        });
+    }
+};
+
+module.exports = { middleware };
