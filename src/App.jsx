692dc5b9f14adf778a4560312f51cb290ce2ff4d
KnightDeveloper123 <KnightDeveloper123.com>
2025-03-28 17:15:15 +0530

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Flex, Spinner } from '@chakra-ui/react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import './App.css'
 
