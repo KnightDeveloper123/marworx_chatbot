b083785c8d57e3b4035a1e7a2981096b2d70cdae
KnightDeveloper123 <developer2@knightmotion.com>
2025-04-18 14:54:25 +0530

    sidebar

--- src/components/user/SideBar.jsx
+++ src/components/user/SideBar.jsx
@@ -1,5 +1,5 @@
-import React,{  useContext, useEffect, useState } from "react";
-import { Box, Button,  Flex, Heading, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Portal, Stack, Text } from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { Box, Button, Flex, Heading, IconButton, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger, Portal, Stack, Text } from "@chakra-ui/react";
 import { DeleteIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { FaRobot } from "react-icons/fa";
 import axios from "axios";
@@ -12,7 +12,7 @@ const SideBar = () => {
   const [isOpen, setIsOpen] = useState(true);
   const [sideData, setSideData] = useState([]);
 
-  const {clearChat } = useContext(AppContext);
+  const { clearChat } = useContext(AppContext);
 
   const { id } = useParams();
   const { userid } = useParams();
@@ -52,9 +52,9 @@ const SideBar = () => {
     try {
       await axios.delete(`${APP_URL}/chatbot/deleteChatTitle`, {
         headers: { Authorization: `${token}` },
-        data: { title_id: deleteId }, 
+        data: { title_id: deleteId },
       });
-      
+
 
       getData();
       navigate(`/${userid}`);
@@ -70,119 +70,132 @@ const SideBar = () => {
 
     getData();
 
-  }, [id,clearChat]);
+  }, [id, clearChat]);
 
   return (
     <>
-    <Box display="flex" >
-      <Box
-        h="100vh"
-        w={isOpen ? "250px" : "0px"}
-        overflow="hidden"
-        bgColor="#171923"
-        p={isOpen ? "4" : "0"}
-        color="white"
-        transition="width 0.5s"
-        z-index={9999}
-        position={{base: "absolute", md: "unset"}}
-        overflowY={"auto"}
-        sx={{
-          "&::-webkit-scrollbar": {
+      <Box display="flex" >
+        <Box
+          h="100vh"
+          w={isOpen ? "250px" : "0px"}
+          overflow="hidden"
+          bgColor="#171923"
+          p={isOpen ? "4" : "0"}
+          color="white"
+          transition="width 0.5s"
+          z-index={9999}
+          position={{ base: "absolute", md: "unset" }}
+          overflowY={"auto"}
+          sx={{
+            "&::-webkit-scrollbar": {
               width: "8px",
-          },
-          "&::-webkit-scrollbar-track": {
+            },
+            "&::-webkit-scrollbar-track": {
               background: "#2D3748",
               borderRadius: "10px",
-          },
-          "&::-webkit-scrollbar-thumb": {
+            },
+            "&::-webkit-scrollbar-thumb": {
               background: "#4A5568",
               borderRadius: "10px",
-          },
-          "&::-webkit-scrollbar-thumb:hover": {
+            },
+            "&::-webkit-scrollbar-thumb:hover": {
               background: "#718096",
-          },
-      }}
-      >
+            },
+          }}
+        >
 
-      
-        {isOpen && (
+
+          {isOpen && (
+            <IconButton
+              icon={<HamburgerIcon boxSize={6} />}
+              colorScheme="whiteAlpha"
+              variant="ghost"
+              onClick={() => setIsOpen(false)}
+            />
+          )}
+
+          {isOpen && (
+            <Box ml="10px" >
+              <Flex gap="2" align="center">
+                <FaRobot />
+                <Heading size="sm">ChatBot</Heading>
+              </Flex>
+
+              <Stack ml="10px" mt="15px" >
+                <Text fontSize="sm">Today</Text>
+                <Stack whiteSpace='nowrap' textOverflow='ellipsis' overflow='hidden' >
+                  {sideData.map((data) =>
+                    <Text onClick={() => {
+                      partData(data.id);
+                    }}
+                      cursor="pointer"
+                      fontSize="xs"
+                      display={"flex"}
+
+                      justifyContent={"space-between"}
+                      key={data.id}>{data.title}
+                      {/* <DeleteIcon onClick={() => handleDelete(data.id)} cursor="pointer" color={"#FF0000"} /> */}
+
+                      <Popover>
+                        <PopoverTrigger>
+                          <DeleteIcon cursor="pointer" color="#FF0000" />
+                        </PopoverTrigger>
+                        <Portal>
+                          <Box zIndex="99999" position="relative">
+                            <PopoverContent
+                              position="absolute"
+                              zIndex="inherit"
+                              w="250px"
+
+                              ml={'-100px'}
+                              h="120px"
+                              bg="#171923"
+                              color="white"
+                              border="none"
+                              boxShadow="lg"
+                              borderRadius="md"
+                            >
+                              <PopoverArrow bgColor="#171923" />
+                              <PopoverHeader bgColor="#171923" border="none" />
+                              <PopoverCloseButton color="white" />
+                              <PopoverBody
+                                bgColor="#171923"
+                                display="flex"
+                                flexDirection="column"
+                              >
+                                <Text color="white">Do You want to delete?</Text>
+                                <Button colorScheme="red" onClick={() => handleDelete(data.id)}>
+                                  Delete
+                                </Button>
+                              </PopoverBody>
+                            </PopoverContent>
+                          </Box>
+                        </Portal>
+                      </Popover>
+
+                    </Text>
+
+                  )}
+
+                </Stack>
+              </Stack>
+            </Box>
+          )}
+        </Box>
+
+        {!isOpen && (
           <IconButton
             icon={<HamburgerIcon boxSize={6} />}
             colorScheme="whiteAlpha"
             variant="ghost"
-            onClick={() => setIsOpen(false)}
+            onClick={() => setIsOpen(true)}
+            position="absolute"
+            top="16px"
+            left="16px"
+            zIndex="10"
           />
         )}
-
-        {isOpen && (
-          <Box ml="10px">
-            <Flex gap="2" align="center">
-              <FaRobot />
-              <Heading size="sm">ChatBot</Heading>
-            </Flex>
-
-            <Stack ml="10px" mt="15px">
-              <Text fontSize="sm">Today</Text>
-              <Stack whiteSpace='nowrap' textOverflow='ellipsis' overflow='hidden' >
-                {sideData.map((data) =>
-                  <Text onClick={() => {
-                    partData(data.id);
-                  }}
-                    cursor="pointer"
-                    fontSize="xs"
-                    display={"flex"}
-                    justifyContent={"space-between"}
-                    key={data.id}>{data.title}
-                    {/* <DeleteIcon onClick={() => handleDelete(data.id)} cursor="pointer" color={"#FF0000"} /> */}
-
-                    <Popover >
-                      <PopoverTrigger bgColor={"#black"}>
-                        <DeleteIcon  cursor="pointer" color={"#FF0000"} />
-                      </PopoverTrigger>
-                      <Portal bgColor={"#171923"}>
-                        <PopoverContent w="250px"
-                          h="120px"
-                          bg="#171923"  
-                          color="white"  
-                          border="none"  
-                          boxShadow="lg"
-                          borderRadius="md"
-                        >
-                          <PopoverArrow bgColor={"#171923"} />
-                          <PopoverHeader bgColor={"#171923"} border={"none"}></PopoverHeader>
-                          <PopoverCloseButton color={"white"} />
-                          <PopoverBody bgColor={"#171923"} display="flex" flexDirection="column"   >
-                            <Text color={"white"}> Do You want to delete? </Text>
-                            <Button colorScheme='red' onClick={() => handleDelete(data.id)}>Delete</Button>
-                          </PopoverBody>
-
-                        </PopoverContent>
-                      </Portal>
-                    </Popover>
-
-                  </Text>
-
-                )}
-
-              </Stack>
-            </Stack>
-          </Box>
-        )}
       </Box>
-
-      {!isOpen && (
-        <IconButton
-          icon={<HamburgerIcon boxSize={6} />}
-          colorScheme="whiteAlpha"
-          variant="ghost"
-          onClick={() => setIsOpen(true)}
-          position="absolute"
-          top="16px"
-          left="16px"
-          zIndex="10"
-        />
-      )}
-    </Box>
     </>
   );
 };
